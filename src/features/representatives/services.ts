db38976e9f3f166c750953826fd5875ad86dce77
CarlUdo <carl@wbsweden.com>
2024-11-22 19:46:46 +0100

    Make representatives add service function

--- src/features/representatives/services.ts
+++ src/features/representatives/services.ts
@@ -2,12 +2,19 @@ import { newRepresentativeSchema } from "./validation";
 
 const db = [
   {
+    id: 1,
     name: "Carl",
     email: "carl@wbsweden.com"
   },
   {
+    id: 2,
     name: "Andrea",
     email: "a@hotmail.com"
+  },
+  {
+    id: 3,
+    name: "Chriss",
+    email: "c@hotmail.com"
   },
 ];
 
@@ -15,8 +22,9 @@ export const createService = () => {
   return {
     getAll: async () => db,
     add: async (rawData: {name: string, email: string}) => {
-      const represenative = newRepresentativeSchema.parse({ rawData });
-      db.push(represenative);
+      const represenative = newRepresentativeSchema.parse( rawData );
+      const id = Math.floor(Math.random() * 100000);
+      db.push({ id, ...represenative });
     }
   };
 };
