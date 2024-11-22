8ea48625e4e09ee573d7982e182c338166df1705
CarlUdo <carl@wbsweden.com>
2024-11-22 17:40:44 +0100

    Make addRepresentativeAction function

---
+++ src/features/representatives/actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { createService } from "./services";
+
+export const addRepresentativeAction = async (formData: FormData) => {
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+
+  try {
+    await createService().add({ name, email });
+  } catch (error) {
+    console.log(error);
+  }
+  
+  revalidatePath("/representatives");
+};
