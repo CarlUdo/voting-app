6699b4d7c207a5e7839fc780ae971f4828ddfae9
CarlUdo <carl@wbsweden.com>
2024-11-22 16:31:32 +0100

    Make representative schema

---
+++ src/features/representatives/validation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const idSchema = z.object({
+  id: z.number(),
+});
+
+export const newRepresentativeSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+});
+
+export const representativeSchema = idSchema.merge(newRepresentativeSchema);
+
+export type NewRepresentative = z.infer<typeof newRepresentativeSchema>;
+export type Representative = z.infer<typeof representativeSchema>;
