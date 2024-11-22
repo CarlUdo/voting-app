7243102851d5a47501d46e1380cf52b4efd8d023
CarlUdo <carl@wbsweden.com>
2024-11-22 20:13:15 +0100

    Make a remove button component

---
+++ src/features/representatives/ui/remove-button.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { removeRepresentativeAction } from "../actions";
+
+type Props = {
+  id: number;
+};
+
+export function RemoveButton({ id }: Props) {
+  return (
+    <button onClick={() => removeRepresentativeAction(id)} className="absolute top-1 right-1 text-red-600 text-2xl"> &times; </button>
+  );
+}
