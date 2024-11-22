9f0f423da84872353cad1e499ce733da7bc5d183
CarlUdo <carl@wbsweden.com>
2024-11-22 20:15:31 +0100

    Run prettier

--- src/features/representatives/actions.ts
+++ src/features/representatives/actions.ts
@@ -25,4 +25,3 @@ export const removeRepresentativeAction = async (id: number) => {
 
   revalidatePath("/representatives");
 };
-
--- src/features/representatives/ui/remove-button.tsx
+++ src/features/representatives/ui/remove-button.tsx
@@ -8,6 +8,12 @@ type Props = {
 
 export function RemoveButton({ id }: Props) {
   return (
-    <button onClick={() => removeRepresentativeAction(id)} className="absolute top-1 right-1 text-red-600 text-2xl"> &times; </button>
+    <button
+      onClick={() => removeRepresentativeAction(id)}
+      className="absolute top-1 right-1 text-red-600 text-2xl"
+    >
+      {" "}
+      &times;{" "}
+    </button>
   );
 }
