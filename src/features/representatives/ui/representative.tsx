0ca8ba27fe685886b573e0fdbaae7a619c39f74b
CarlUdo <carl@wbsweden.com>
2024-11-23 12:31:35 +0100

    Remove email from rendering

--- src/features/representatives/ui/representative.tsx
+++ src/features/representatives/ui/representative.tsx
@@ -5,7 +5,7 @@ export function Representative({ data }: { data: RepresentativeType }) {
   return (
     <div className="card shadow-md p-4 bg-white rounded-lg border border-gray-200 min-w-80 max-w-fit">
       <RemoveButton id={data.id} />
-      <h2 className="text-xl font-semibold">{data.name} ({data.email})</h2>
+      <h2 className="text-xl font-semibold">{data.name}</h2>
     </div>
   );
 }
