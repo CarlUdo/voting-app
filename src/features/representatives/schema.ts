77a672d0d9b99ca175b885d0693dbd158becfc92
CarlUdo <carl@wbsweden.com>
2024-11-23 12:17:28 +0100

    Make public voters schema

--- src/features/representatives/schema.ts
+++ src/features/representatives/schema.ts
@@ -8,8 +8,8 @@ export const represenativesTable = pgTable("represenatives", {
   email: varchar({ length: 255 }).notNull().unique(),
 });
 
-// export const usersTable = pgTable("users", {
-//   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-//   username: varchar({ length: 255 }).notNull(),
-//   email: varchar({ length: 255 }).notNull().unique(),
-// });
+export const publicVotersTable = pgTable("public_voters", {
+  id: pgUuid().primaryKey().default(v4()),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
