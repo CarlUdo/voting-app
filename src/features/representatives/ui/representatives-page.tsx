e3a6029eadacfba02c77c10634b3d77804f7a3d9
CarlUdo <carl@wbsweden.com>
2024-11-23 15:56:42 +0100

    Add Heading component

--- src/features/representatives/ui/representatives-page.tsx
+++ src/features/representatives/ui/representatives-page.tsx
@@ -1,9 +1,11 @@
+import { Heading } from "@/ui/heading";
 import { InputForm } from "./input-form";
 import { RepresentativeBoard } from "./representative-board";
 
 export function RepresenativesPage() {
   return (
     <div>
+      <Heading title="Vote for your representative" /> 
       <InputForm />
       <RepresentativeBoard />
     </div>
