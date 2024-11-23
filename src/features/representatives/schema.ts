f16f2e26903cfe6f5b17266628fa4ab8035101bb
CarlUdo <carl@wbsweden.com>
2024-11-23 09:58:59 +0100

    Make users schema

---
+++ src/features/representatives/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar, boolean as pgBoolean } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  username: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  isRepresentative: pgBoolean().default(false),
+});
