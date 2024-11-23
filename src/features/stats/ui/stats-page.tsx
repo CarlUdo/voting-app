958bfee9fdba36f8d96abc5186336bce60c8c079
CarlUdo <carl@wbsweden.com>
2024-11-23 17:41:39 +0100

    Make stats component

---
+++ src/features/stats/ui/stats-page.tsx
@@ -0,0 +1,14 @@
+import { Heading } from "@/ui";
+
+export function StatsPage() {
+  return (
+    <main>
+      <header>
+        <Heading title="Statistics" />
+      </header>
+      <section>
+        Statistics
+      </section>
+    </main>
+  );
+}
