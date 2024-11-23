2a8b36ceecbd95debff884ab1dee8754cdd6bb7c
CarlUdo <carl@wbsweden.com>
2024-11-23 15:24:22 +0100

    Make RepresentativesPage component

---
+++ src/features/representatives/ui/representatives-page.tsx
@@ -0,0 +1,11 @@
+import { InputForm } from "./input-form";
+import { RepresentativeBoard } from "./representative-board";
+
+export function RepresenativesPage() {
+  return (
+    <div>
+      <InputForm />
+      <RepresentativeBoard />
+    </div>
+  );
+}
