bf759071483c0e23ffbc3c9402645b69e9671d96
CarlUdo <carl@wbsweden.com>
2024-11-23 17:29:48 +0100

    Make issues component

---
+++ src/features/issues/issues-page.tsx
@@ -0,0 +1,15 @@
+import { Heading } from "@/ui";
+
+
+export function IssuesPage() {
+  return (
+    <main>
+      <header>
+        <Heading title="Vote for your representative" />
+      </header>
+      <section>
+        Issues
+      </section>
+    </main>
+  );
+}
