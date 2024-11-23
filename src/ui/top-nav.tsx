a3401437eda95caec11de36cd9f4d989893e380d
CarlUdo <carl@wbsweden.com>
2024-11-23 15:21:17 +0100

    Make TopNav Component

---
+++ src/ui/top-nav.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+const links = [
+  {
+    name: "Representatives",
+    href: "/representatives",
+  },
+  {
+    name: "Stats",
+    href: "/stats",
+  },
+  
+  {
+    name: "Logout",
+    href: "/",
+  },
+];
+
+export function TopNav() {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-end">
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              "ml-2", {
+              "text-blue-700": pathname === link.href,
+            })}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
