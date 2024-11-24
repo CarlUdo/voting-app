9568122c43de8bfd2f69ea8811a044b87c89130c
CarlUdo <carl@wbsweden.com>
2024-11-24 15:51:49 +0100

    Style public-voting representative board

--- src/features/public-voting/ui/representatives-board.tsx
+++ src/features/public-voting/ui/representatives-board.tsx
@@ -4,13 +4,13 @@ import { PublicVoteCard } from "./public-vote-card";
 export async function RepresentativesBoard() {
   const representatives = await represenativesService.getAll();
   return (
-    <>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {representatives.map((representative) => (
         <PublicVoteCard
           key={representative.id}
           representative={representative}
         />
       ))}
-    </>
+    </div>
   );
 }
