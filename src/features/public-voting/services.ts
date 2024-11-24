0cae922054a37b3ee35165efc28b1457e5f13364
CarlUdo <carl@wbsweden.com>
2024-11-24 18:33:34 +0100

    Make public-voting setvice function

--- src/features/public-voting/services.ts
+++ src/features/public-voting/services.ts
@@ -1,8 +1,19 @@
 import { Db } from "@/db";
-import { publicVotersTable } from "./schema";
+import { publicVotersTable, publicVotesTable } from "./schema";
+import { desc, eq } from "drizzle-orm";
 
 export const createService = (db: Db) => {
   return {
     getAllPublicVoters: async () => await db.select().from(publicVotersTable),
+    getLatestVoteByPublicVoter: async (publicVoterId: string) => {
+      const votes = await db
+        .select()
+        .from(publicVotesTable)
+        .where(eq(publicVotesTable.publicVoterId, publicVoterId))
+        .orderBy(desc(publicVotesTable.dateCreated))
+        .limit(1);
+      
+      return votes[0];
+    },
   };
 };
