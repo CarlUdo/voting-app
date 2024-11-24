e26f90633bbbd319aca40df88e32be29e0a71328
CarlUdo <carl@wbsweden.com>
2024-11-24 09:30:40 +0100

    Update issues schemas

--- src/features/issues/schema.ts
+++ src/features/issues/schema.ts
@@ -1,9 +1,27 @@
 import { pgTable, varchar, uuid as pgUuid, boolean, timestamp } from "drizzle-orm/pg-core";
 import { v4 } from "uuid";
+import { relations } from "drizzle-orm";
 
 export const issuesTable = pgTable("issues", {
   id: pgUuid().primaryKey().default(v4()),
   name: varchar({ length: 255 }).notNull(),
   active: boolean().notNull().default(true),
   dateCreated: timestamp().notNull().defaultNow(),
-}); 
+});
+
+export const choicesTable = pgTable("choices", {
+  id: pgUuid().primaryKey().default(v4()),
+  issueId: pgUuid().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const issuesRelations = relations(issuesTable, ({ many }) => ({
+  choices: many(choicesTable),
+}));
+
+export const choicesRelations = relations(choicesTable, ({ one }) => ({
+  issue: one(issuesTable, {
+    fields: [choicesTable.issueId],
+    references: [issuesTable.id],
+  }),
+})); 
