ae0d79db6a03c6291a2dd93f93a97446a4158d09
CarlUdo <carl@wbsweden.com>
2024-11-24 19:18:14 +0100

    Update public-vote-card

--- src/features/public-voting/ui/public-vote-card.tsx
+++ src/features/public-voting/ui/public-vote-card.tsx
@@ -1,12 +1,24 @@
 import { RepresentativeType } from "@/libs";
+import clsx from "clsx";
+import { VoteForm } from "./vote-form";
 
 type Props = {
   representative: RepresentativeType;
+  publicVoterId: string;                
+  isCurrentVote: boolean;
 };
 
-export function PublicVoteCard({ representative }: Props) {
+export function PublicVoteCard({ representative, publicVoterId, isCurrentVote }: Props) {
   return (
-    <article className="card shadow-md p-4 bg-white rounded-lg border">
+    <article 
+      className={clsx(
+        "card shadow-md p-4 bg-white rounded-lg border",
+        {
+          "border-blue-500 border-2": isCurrentVote,
+          "border-gray-200": !isCurrentVote,
+        }
+      )}
+    >
       <div>
         <div className="flex flex-col gap-4">
           <header className="flex justify-between items-start">
@@ -15,6 +27,9 @@ export function PublicVoteCard({ representative }: Props) {
               <p className="text-sm text-gray-500">{representative.email}</p>
             </div>
           </header>
+          <footer>
+            <VoteForm publicVoterId={publicVoterId} representativeId={representative.id} />
+          </footer>
         </div>
       </div>
     </article>
