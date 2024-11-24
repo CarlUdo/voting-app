2e04e0c2bdf647e3a273046fe7f313cfd49bdf2e
CarlUdo <carl@wbsweden.com>
2024-11-24 15:40:29 +0100

    Fetch voters from public-voting page

--- src/features/public-voting/ui/public-voting-page.tsx
+++ src/features/public-voting/ui/public-voting-page.tsx
@@ -1,15 +1,16 @@
 import { Heading } from "@/ui";
 import { PublicVotingSelector, RepresentativesBoard } from ".";
+import { publicVotingService } from "../instance";
 
-
-export function PublicVotingPage() {
+export async function PublicVotingPage() {
+  const publicVoters = await publicVotingService.getAllPublicVoters();
   return (
     <main>
       <header>
         <Heading title="Vote for your representative" />
       </header>
       <section className="p-4">
-        <PublicVotingSelector />        
+        <PublicVotingSelector publicVoters={publicVoters} />        
       </section>
       <RepresentativesBoard />
     </main>
--- src/features/public-voting/ui/public-voting-selector.tsx
+++ src/features/public-voting/ui/public-voting-selector.tsx
@@ -1,15 +1,17 @@
-import { publicVotingService } from "../instance";
+import type { PublicVoter } from "../validation";
 
-export async function PublicVotingSelector() {
-  const voters = await publicVotingService.getAllPublicVoters();
+type Props = {
+  publicVoters: PublicVoter[];
+};
 
+export async function PublicVotingSelector({ publicVoters }: Props) {  
   return (
     <div className="form-control w-full max-w-xs mb-8">
       <select 
         className="select select-bordered"
       >
         <option value="">Select a voter...</option>
-        {voters.map((voter) => (
+        {publicVoters.map((voter) => (
           <option key={voter.id} value={voter.id}>
             {voter.name}
           </option>
