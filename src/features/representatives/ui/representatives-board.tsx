c225836af9564df57999018c459496649b55a7f9
CarlUdo <carl@wbsweden.com>
2024-11-24 07:36:24 +0100

    Change name

---
+++ src/features/representatives/ui/representatives-board.tsx
@@ -0,0 +1,16 @@
+import { represenativesService } from "../instance";
+import { Representative } from ".";
+
+export async function RepresentativesBoard() {
+  const representatives = await represenativesService.getAll();
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Representatives</h1>
+      <div className="grid grid-cols-1 gap-4">
+        {representatives.map((representative) => (
+          <Representative key={representative.id} data={representative} />
+        ))}
+      </div>
+    </div>
+  );
+}
