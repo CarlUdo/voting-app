0bfa0c7464a6c29a799ab7ccfa61dfe0a1a0d4e1
CarlUdo <carl@wbsweden.com>
2024-11-24 19:18:37 +0100

    Update barrel file

--- src/features/public-voting/ui/index.ts
+++ src/features/public-voting/ui/index.ts
@@ -2,3 +2,4 @@ export * from "./public-voting-page";
 export * from "./representatives-board";
 export * from "./public-voting-selector";
 export * from "./public-vote-card";
+export * from "./vote-form";
