e1903f262c3992505a5bb28d32f262bcec3fd927
CarlUdo <carl@wbsweden.com>
2024-11-24 09:32:11 +0100

    Update issues service

--- src/features/issues/services.ts
+++ src/features/issues/services.ts
@@ -1,17 +1,27 @@
 import { Db } from "@/db";
-import { issuesTable } from "./schema";
+import { choicesTable, issuesTable } from "./schema";
 import { NewIssue, newIssueSchema } from "./validation";
 import { v4 } from "uuid";
 import { eq } from "drizzle-orm";
 
 export const createService = (db: Db) => {
   return {
-    getAll: async () => await db.select().from(issuesTable),
+    getAll: async () => {
+      const issues = await db.select().from(issuesTable);
+      const choices = await db.select().from(choicesTable);
+      
+      return issues.map(issue => ({
+        ...issue,
+        choices: choices.filter(choice => choice.issueId === issue.id),
+      }));
+    },
     add: async (rawData: NewIssue) => {
       const issue = newIssueSchema.parse(rawData);
-      await db
-        .insert(issuesTable)
-        .values({ id: v4(), ...issue });
+      const issueId = v4();
+      await db.insert(issuesTable).values({ id: issueId, name: issue.name, active: true, }); 
+      if (issue.choices.length > 0) { 
+        await db.insert(choicesTable).values( issue.choices.map(choice => ({ id: v4(), issueId, name: choice.name, })) ); 
+      }
     },
     updateActive: async (id: string, active: boolean) => {
       await db
