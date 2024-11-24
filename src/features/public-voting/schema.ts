fe6e6cfe65a0b0aa96706b6d4d8b61d04b0e7d89
CarlUdo <carl@wbsweden.com>
2024-11-24 15:23:03 +0100

    Update folder path

---
+++ src/features/public-voting/schema.ts
@@ -0,0 +1,20 @@
+import {
+  pgTable,
+  uuid as pgUuid,
+  timestamp,
+  varchar
+} from "drizzle-orm/pg-core";
+import { v4 } from "uuid";
+
+export const publicVotesTable = pgTable("public_votes", {
+  id: pgUuid().primaryKey().default(v4()),
+  publicVoterId: pgUuid().notNull(),
+  representativeId: pgUuid().notNull(),
+  dateCreated: timestamp().notNull().defaultNow(),
+});
+
+export const publicVotersTable = pgTable("public_voters", {
+  id: pgUuid().primaryKey().default(v4()),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
--- src/features/representatives/schema.ts
+++ src/features/representatives/schema.ts
@@ -6,9 +6,3 @@ export const represenativesTable = pgTable("represenatives", {
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
-
-export const publicVotersTable = pgTable("public_voters", {
-  id: pgUuid().primaryKey().default(v4()),
-  name: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
