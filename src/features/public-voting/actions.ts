c3c11e269739965cfe15bf6d3b1794994e7bb5c5
CarlUdo <carl@wbsweden.com>
2024-11-24 19:19:07 +0100

    Make add public-vote action

---
+++ src/features/public-voting/actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { publicVotingService } from "./instance";
+
+export const addPublicVoteAction = async (formData: FormData) => {
+  const publicVoterId = formData.get("publicVoterId") as string;
+  const representativeId = formData.get("representativeId") as string;
+
+  try {
+    await publicVotingService.add({ publicVoterId, representativeId });
+  } catch (error) {
+    console.log(error);
+  }
+
+  revalidatePath("/public-voting");
+}; 
