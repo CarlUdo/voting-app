12e3437bda9eac0652b99733c9f881c0a4d0b901
CarlUdo <carl@wbsweden.com>
2024-11-24 13:58:20 +0100

    Update public voting component

--- src/features/public-voting/ui/public-voting-page.tsx
+++ src/features/public-voting/ui/public-voting-page.tsx
@@ -1,7 +1,36 @@
+import { Heading } from "@/ui";
+
 export function PublicVotingPage() {
+  const voters = [
+    {
+      id: 1,
+      name: 'carl'
+    },
+    {
+      id: 2,
+      name: 'Andreas'
+    },
+  ];
+
   return (
-    <p>
-      Hello from voting page
-    </p>
+    <main>
+      <header>
+        <Heading title="Vote for your representative" />
+      </header>
+      <section className="p-4">
+        <div className="form-control w-full max-w-xs mb-8">
+          <select 
+            className="select select-bordered"
+          >
+            <option value="">Select a voter...</option>
+            {voters.map((voter) => (
+              <option key={voter.id} value={voter.id}>
+                {voter.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </section>
+    </main>
   );
 }
