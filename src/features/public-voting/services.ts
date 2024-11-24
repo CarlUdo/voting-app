9bcce1ab53c5c2ec12c7cb31023f464436d1fd2b
CarlUdo <carl@wbsweden.com>
2024-11-24 15:26:02 +0100

    Make public-voting service file

---
+++ src/features/public-voting/services.ts
@@ -0,0 +1,8 @@
+import { Db } from "@/db";
+import { publicVotersTable } from "./schema";
+
+export const createService = (db: Db) => {
+  return {
+    getAllPublicVoters: async () => await db.select().from(publicVotersTable)
+  };
+};
