106debc10578f068a3343c5fa02e07bb2ae07f09
CarlUdo <carl@wbsweden.com>
2024-11-24 19:53:25 +0100

    Update votig page component

--- src/features/representative-voting/ui/voting-page.tsx
+++ src/features/representative-voting/ui/voting-page.tsx
@@ -1,5 +1,25 @@
-export function VotingPage() {
+import { represenativesService } from "@/features/representatives";
+import { Heading } from "@/ui";
+import { Suspense } from "react";
+import { RepresentativeVotingSelector } from "./representative-voting-selector";
+
+export async function VotingPage() {
+  const representatives = await represenativesService.getAll();
+  
   return (
-    <p>Voting page</p>
+    <main>
+      <header>
+        <Heading title="Vote for your representative" />
+      </header>
+      <section className="p-4">
+        <RepresentativeVotingSelector representatives={representatives} />
+      </section>
+      {/* <Suspense fallback={<div>Loading...</div>}>
+        <RepresentativesBoard
+          publicVoterId={selectedVoterId}
+          currentVote={currentVote}
+        />
+      </Suspense> */}
+    </main>
   );
 }
