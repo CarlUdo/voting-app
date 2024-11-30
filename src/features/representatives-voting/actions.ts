614653b6d63f441f8408c26a7fa394ee144a8b9b
CarlUdo <carl@wbsweden.com>
2024-11-30 19:47:08 +0100

    Make addRepresentativeVoteAction

---
+++ src/features/representatives-voting/actions.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { representativesVotingService } from ".";
+
+export const addRepresentativeVoteAction = async (formData: FormData) => {
+  const representativeId = formData.get("representativeId") as string;
+  const issueId = formData.get("issueId") as string;
+  const choiceId = formData.get("choiceId") as string;
+
+  try {
+    await representativesVotingService.add({ 
+      representativeId, 
+      issueId,
+      choiceId 
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  revalidatePath("/representative-vote");
+}; 
