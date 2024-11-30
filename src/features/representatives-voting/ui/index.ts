bb053be84ccd93c322380f79dab9e36bfc3372cc
CarlUdo <carl@wbsweden.com>
2024-11-30 16:30:40 +0100

    Update barrel file

--- src/features/representatives-voting/ui/index.ts
+++ src/features/representatives-voting/ui/index.ts
@@ -1,3 +1,4 @@
+export * from "./issues-board";
+export * from "./issues-vote-card";
 export * from "./representatives-voting-page";
 export * from "./representatives-voting-selector";
-export * from "./issues-board";
