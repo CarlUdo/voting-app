fb639d89e9193916de3e75d5a77a7d60c6e979a9
CarlUdo <carl@wbsweden.com>
2024-11-30 12:15:11 +0100

    Update links

--- src/ui/top-nav.tsx
+++ src/ui/top-nav.tsx
@@ -89,6 +89,7 @@ export function TopNav() {
                     href={link.href}
                     className={clsx({
                       "text-blue-700": pathname === link.href,
+                      "text-red-600": link.href === "/",
                     })}
                   >
                     {link.name}
@@ -115,6 +116,7 @@ export function TopNav() {
                   "block px-3 py-2 rounded-md text-base font-medium",
                   {
                     "text-blue-700": pathname === link.href,
+                    "text-red-600": link.href === "/",
                   },
                 )}
               >
