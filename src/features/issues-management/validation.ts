f411bc1f168ff32d9c42315d52f4adc12d6c15c0
CarlUdo <carl@wbsweden.com>
2024-12-01 13:53:38 +0100

    Remove type

--- src/features/issues-management/validation.ts
+++ src/features/issues-management/validation.ts
@@ -28,6 +28,5 @@ export const issueSchema = idSchema.merge(
 );
 
 export type NewIssue = z.infer<typeof newIssueSchema>;
-export type IssueType = z.infer<typeof issueSchema>;
 export type NewChoice = z.infer<typeof newChoiceSchema>;
 export type Choice = z.infer<typeof choiceSchema>;
