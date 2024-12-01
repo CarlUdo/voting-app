14084d3855e6fffe99539d4f53b09a4b5c39f3a4
CarlUdo <carl@wbsweden.com>
2024-12-01 09:02:56 +0100

    Break out props

--- src/features/representatives-management/ui/representative.tsx
+++ src/features/representatives-management/ui/representative.tsx
@@ -1,6 +1,10 @@
 import { RepresentativeType } from "@/libs";
 
-export function Representative({ data }: { data: RepresentativeType }) {
+type Props = { 
+  data: RepresentativeType;
+};
+
+export function Representative({ data }: Props) {
   return (
     <div className="card shadow-lg px-4 py-3 bg-white rounded-xl border border-gray-200">
       <h2 className="text-xl font-semibold mb-2 text-gray-800">{data.name}</h2>
