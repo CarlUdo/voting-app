c866d5f23b53973eb7c88c6ae9506369df39a956
CarlUdo <carl@wbsweden.com>
2024-12-01 11:30:58 +0100

    Update seed file

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,16 +2,21 @@ import { represenativesTable } from "@/features/representatives-management/schem
 import { db } from ".";
 import { getPeople } from "./seed-helpers";
 import { publicVotersTable } from "@/features/public-voting";
+import { represenativesService } from "@/features/representatives-management";
+
+const TOTAL_NUMBER_OF_PEOPLE = 100;
+const NUMBER_OF_REPRESENTATIVES = 10;
 
 const seed = async () => {
-  const people = getPeople(50);
-  const numberOfRepresenatives = 10;
+  console.log("Starting seed process...");
+  const people = getPeople(TOTAL_NUMBER_OF_PEOPLE);
 
   try {
-    await db.delete(represenativesTable);
+    console.log("Clearing existing data...");
+    await represenativesService.deleteTable();
     await db
       .insert(represenativesTable)
-      .values(people.slice(0, numberOfRepresenatives));
+      .values(people.slice(0, NUMBER_OF_REPRESENTATIVES));
 
     await db.delete(publicVotersTable);
     await db.insert(publicVotersTable).values(people);
