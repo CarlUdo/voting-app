2f26d655fdf7b0200cf43941790fd5a1ecafebff
CarlUdo <carl@wbsweden.com>
2024-12-01 08:53:33 +0100

    Remove comment

--- src/features/issues-management/ui/issues-page.tsx
+++ src/features/issues-management/ui/issues-page.tsx
@@ -16,37 +16,3 @@ export function IssuesPage() {
     </main>
   );
 }
-
-// export function RepresenativesPage() {
-//   return (
-//     <main>
-//       <header>
-//         <Heading title="Representatives management" />
-//       </header>
-//       <section>
-//         <InputForm />
-//       </section>
-//       <section>
-//         <RepresentativesBoard />
-//       </section>
-//     </main>
-//   );
-// }
-
-//import { Heading } from "@/ui";
-//import { InputForm, IssuesBoard } from ".";
-
-// export function IssuesPage() {
-//   return (
-//     <main>
-//       Hej jej
-//     </main>
-//   );
-// }
-
-// return (
-//   <main>
-
-//     hej hej
-//   </main>
-// );
