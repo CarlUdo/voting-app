38224fe9fa4a760a8a4f6dbc02c44cec52e8897f
CarlUdo <carl@wbsweden.com>
2024-12-08 14:53:28 +0100

    Remove schemas relations

--- src/features/representatives-voting/schema.ts
+++ src/features/representatives-voting/schema.ts
@@ -1,8 +1,5 @@
 import { pgTable, uuid as pgUuid, timestamp } from "drizzle-orm/pg-core";
 import { v4 } from "uuid";
-import { relations } from "drizzle-orm";
-import { represenativesTable } from "../representatives/schema"; // Can't generate drizzle tables if I import from "../representatives-management" - why??
-import { choicesTable, issuesTable } from "../issues/schema"; // Can't generate drizzle tables if I import from "../representatives-management" - why??
 
 export const representativeVotesTable = pgTable("representative_votes", {
   id: pgUuid().primaryKey().default(v4()),
@@ -12,22 +9,4 @@ export const representativeVotesTable = pgTable("representative_votes", {
   dateCreated: timestamp().notNull().defaultNow(),
 });
 
-// Ej på databasnivå utan på serivcenivå!
 
-export const representativeVotesRelations = relations(
-  representativeVotesTable,
-  ({ one }) => ({
-    representative: one(represenativesTable, {
-      fields: [representativeVotesTable.representativeId],
-      references: [represenativesTable.id],
-    }),
-    issue: one(issuesTable, {
-      fields: [representativeVotesTable.issueId],
-      references: [issuesTable.id],
-    }),
-    choice: one(choicesTable, {
-      fields: [representativeVotesTable.choiceId],
-      references: [choicesTable.id],
-    }),
-  }),
-);
